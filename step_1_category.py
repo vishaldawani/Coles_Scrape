f094a3705729537ed3c9e5b505b2081befe69e3e
vishaldawani <34950177+vishaldawani@users.noreply.github.com>
2021-05-24 09:36:48 +1000

    Update step_1_category.py

--- step_1_category.py
+++ step_1_category.py
@@ -18,6 +18,8 @@ provided to us.
 """
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as soup
 from time import sleep
 import os
@@ -27,9 +29,13 @@ import sys
 import utils
 import random
 import numpy as np
+import config
+
 
 # DECLARE GLOBAL VARIABLES
-OUTPUT_FOLDER="C:\\Users\\61433\\Desktop\\Personal Projects\\Coles\\Coles_Scrape\\Output\\Category_Output"
+URL_FOLDER="C:\\Users\\61433\\Desktop\\Personal Projects\\Coles\\Coles_Scrape\\Locations"
+URL_FILE="Coles_URL.csv"
+OUTPUT_FOLDER="C:\\Users\\61433\\Desktop\\Personal Projects\\Coles\\Coles_Scrape\\Output"
 URL_SCRAPE_COUNT=0        
 BASE_COLES_WEBSITE="https://www.coles.com.au/"
 IMPLICIT_WAIT=[1,2,5]
@@ -37,12 +43,8 @@ CHROME_DRIVER_PATH="C:\\Users\\61433\\Desktop\\Personal Projects\\Coles\\Coles_S
 
 
 class Coles_Spider_URL:
-	def __init__(self,df,output_name,range_start,range_end):
-		temp_df=df
-		self.df=temp_df.iloc[range_start:range_end+1]
-		self.df.index=np.arange(len(self.df))
-		self.worker_index=output_name
-		print(self.df)
+	def __init__(self,df):
+		self.df=df
 		self.Base_Spider()                          
 
 	def Get_File_Name(self,out_file_name):
@@ -60,6 +62,7 @@ class Coles_Spider_URL:
 			return 1
 		else:
 			return 0
+	# This method Gets All the Department URLS
 
 	def Get_Department_URL(self,webpage_data):
 		temp_list=[]
@@ -71,15 +74,14 @@ class Coles_Spider_URL:
 			if s.find("/everything/browse/")>0 and s.find("?pageNumber")>0:
 				temp_list.append(s)
 		return temp_list
-														 
-	def Get_Category_URL(self,webpage_data,post_code,url_scraped_from):
+
+	# This method Gets All the Category URLS														 
+	def Get_Category_URL(self,webpage_data):
 		temp_cat_url=[]
 		temp_cat_name=[]
 		temp_cat_count=[]
 		dept_name=""
 		temp_dept_name=[]
-		temp_post_code=[]
-		temp_url_scraped_from=[]
 		sleep(3)
 		if len(webpage_data)==0:
 			return None
@@ -105,93 +107,284 @@ class Coles_Spider_URL:
 						temp_cat_count.append(count.text)
 					temp_cat_url.append(step_5['href'])
 					temp_dept_name.append(dept_name) 		#Update
-					temp_post_code.append(post_code)
-					temp_url_scraped_from.append(url_scraped_from)
 		
-		return temp_url_scraped_from,temp_post_code,temp_dept_name,temp_cat_name,temp_cat_count,temp_cat_url
+		return temp_dept_name,temp_cat_name,temp_cat_count,temp_cat_url
 
-	def PopulateLists(self,data_list):
-		temp_list=[]
-		if isinstance(data_list, list):
-			for num in range(len(data_list)):
-				for item in data_list[num]:
-					temp_list.append(item)
+	# This method gets all the Sub_Cat_URLS
+	def Get_Sub_Cat_URL(self,category_url_data):
+		if len(category_url_data)==0:
+			raise Exception("Category URL Not Provided")
+		
+		temp_sub_cat_url=[]
+		temp_sub_cat_name=[]
+		temp_sub_cat_count=[]
+		subcatname=""
 
-			return temp_list
-		else:
-			raise Exception ("Invalid List Type Object")
+		sleep(3)
+		if len(category_url_data)==0:
+			return None
+		temp_data=category_url_data.find_all('h2',{"class":"cat-nav-heading"})
+		for step_1 in temp_data:
+			step_2=step_1.find_all("span",{"aria-hidden":"true"})
+			for step_3 in step_2:
+				subcatname=step_3.text
+				if len(subcatname)>0:
+					break
+
+		temp_data=category_url_data.find_all('div',{"id":"cat-nav-list-3"})
+		for t in temp_data:
+			step_2=t.find_all("li",{"class":"cat-nav-item"})
+			for step_3 in step_2:
+				step_4=step_3.find_all("a",{"class":"clear"})
+				for step_5 in step_4:
+					title_span=step_5.find_all("span",{"class":"item-title"})
+					count_span=step_5.find_all("span",{"class":"items-found"})
+					for title in title_span:
+						temp_sub_cat_name.append(title.text)
+					for count in count_span:
+						temp_sub_cat_count.append(count.text)
+					temp_sub_cat_url.append(utils.URL_Adjustment_Categories(step_5['href']))
+		
+		return temp_sub_cat_name,temp_sub_cat_count,temp_sub_cat_url
+
+	def Get_Product_Info(self,webpage_data,url_sub_cat):
+		online_prod_number=[]
+		Online_Prod_Name=[]
+		Online_Brand_Name=[]
+		Comp_Prices=[]
+		Comp_Program=[]
+		HiLoPrice=[]
+
+		def Get_Max_Pages(data):
+			total_count=''
+			sleep(2)			
+			c=data.find_all("div",{"class":"pagination-container"})
+			for item in c:
+				x=item.find_all("span",{"class":"number"})
+				for z in x:
+					total_count=z.text
+
+			if total_count=='':
+				return 1
+			else:
+
+				return np.int64(total_count)
+
+		def Get_Product_Number(data,counter):
+			temp_prod_num=[]
+			soup_data=soup(data,features="lxml")
+			for step_1 in soup_data.find_all('div',{"class":"products"}):
+				if counter==1:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate"})
+				else:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate tile-stagger"})					
+				for step_3 in step_2:
+					step_4=step_3.find_all("div",{"class":"product-main-info"})
+					for step_5 in step_4:
+						step_6_prod=step_5.find("h3",class_="product-title")["data-partnumber"]
+						temp_prod_num.append(step_6_prod)
+			
+			if isinstance(temp_prod_num,list):
+				return temp_prod_num
+			else:
+				raise Exception("Product Numbers not Found")
+
+		def Get_Product_Name(data,counter):
+			Online_Product_Name=[]
+			soup_data=soup(data,features="lxml")
+			for step_1 in soup_data.find_all('div',{"class":"products"}):
+				
+				if counter==1:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate"})
+				else:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate tile-stagger"})		
+
+				for step_3 in step_2:
+					step_4=step_3.find_all("div",{"class":"product-main-info"})
+					for step_5 in step_4:
+						step_6_prod=step_5.find_all("h3",class_="product-title")
+						for step_7 in step_6_prod:
+							article_name=step_7.find_all("span",{"class":"product-name"})
+							for name in article_name:
+								if len(name)>0:
+									Online_Product_Name.append(name.text)
+									break
+
+			if isinstance(Online_Product_Name,list):
+				return Online_Product_Name
+			else:
+				raise Exception("Product Names not Found")
+
+		def Get_Brand_Info(data,counter):
+			Online_Brand_Info=[]
+			soup_data=soup(data,features="lxml")
+			for step_1 in soup_data.find_all('div',{"class":"products"}):
+				if counter==1:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate"})
+				else:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate tile-stagger"})						
+
+				for step_3 in step_2:
+					step_4=step_3.find_all("div",{"class":"product-main-info"})
+					for step_5 in step_4:
+						step_6_prod=step_5.find_all("h3",class_="product-title")
+						for step_7 in step_6_prod:
+							brands=step_7.find_all("span",{"class":"product-brand"})
+							for brand in brands:
+								if len(brand)>0:
+									Online_Brand_Info.append(brand.text)
+									break
+
+			if isinstance(Online_Brand_Info,list):
+				return Online_Brand_Info
+			else:
+				raise Exception("Brands not Found")
+
+		def Get_Comp_Price(data,counter):
+			tempPrices=[]
+			soup_data=soup(data,features="lxml")
+			for step_1 in soup_data.find_all('div',{"class":"products"}):
+				if counter==1:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate"})
+				else:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate tile-stagger"})						
+				for step_3 in step_2:
+					step_4=step_3.find_all("div",{"class":"product-main-info"})
+					for step_5 in step_4:
+						step_6_prod=step_5.find_all("span",class_="product-pricing-info")
+						for step_7 in step_6_prod:
+							step_8=step_7.find_all("span",class_="price-container")
+							for step_9 in step_8:
+								dollar_values=step_9.find_all("span",{"class":"dollar-value"})
+								cent_values=step_9.find_all("span",{"class":"cent-value"})
+								currency_Values=step_9.find_all("span",{"class":"currency-sign"})                    
+								for i in range(len(dollar_values)):
+									tempPrices.append(currency_Values[i].text+dollar_values[i].text+cent_values[i].text)
+            
+			if len(tempPrices)!=0:
+				return tempPrices
+			else:
+				return  Exception ("Invalid competitor Pricing")
+
+		def Get_Comp_Pricing_Program(data,counter):
+			temp_program=[]
+			soup_data=soup(data,features="lxml")			
+			for step_1 in soup_data.find_all("header",{"role":"presentation","class":"product-header"}):
+				if step_1.find("div",class_="product-icons"):
+					step_1_1=step_1.find("div",class_="product-icons")
+					x=step_1_1.contents[2]
+					x1=x['class']
+					for y in x1:
+						if y=="icon":
+							pass
+						else:
+							temp_program.append(y)
+				else:
+					temp_program.append("white")		
+
+			if len(temp_program)!=0:
+				return temp_program
+			else:
+				return None
+
+		def Get_Hi_Lo_Price(data,counter):
+			temp_hi_lo_price=[]
+			soup_data=soup(data,features="lxml")
+			sleep(2)
+			for step_1 in soup_data.find_all('div',{"class":"products"}):
+				if counter==1:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate"})
+				else:
+					step_2=step_1.find_all("div",{"class":"colrs-animate tile-animate tile-stagger"})                       
+				for step_3 in step_2:
+					step_4=step_3.find_all("div",{"class":"product-main-info"})
+					for step_5 in step_4:
+						step_6_prod=step_5.find_all("span",class_="product-pricing-info")
+						for step_7 in step_6_prod:
+							step_8=step_7.find_all("span",class_="saving-container")
+							if len(step_8)==0:
+								temp_hi_lo_price.append("NULL")
+							else:
+								for step_9 in step_8:
+									span_0=step_9.find_all("span",{"class":"product-save-value"})
+									for span_1 in span_0:
+										span_2=span_1.stripped_strings
+										for span_3 in span_2:
+											temp_hi_lo_price.append(span_3)
+
+			return temp_hi_lo_price
+
+		max_page_count=Get_Max_Pages(data=webpage_data)
+		
+		for count in range(1,max_page_count+1):
+			if count==1:				# Because we are already on the first website
+				pass
+			else:
+				temp_urlx=url_sub_cat[:-len(str(count))]+str(count)				
+				self.browser.get(temp_urlx)  
+				sleep(2) 
+
+			online_prod_number.append(Get_Product_Number(data=self.browser.page_source,counter=count))
+			Online_Prod_Name.append(Get_Product_Name(data=self.browser.page_source,counter=count))
+			Online_Brand_Name.append(Get_Brand_Info(data=self.browser.page_source,counter=count))
+			Comp_Prices.append(Get_Comp_Price(data=self.browser.page_source,counter=count))
+			Comp_Program.append(Get_Comp_Pricing_Program(data=self.browser.page_source,counter=count))
+			HiLoPrice.append(Get_Hi_Lo_Price(data=self.browser.page_source,counter=count))
+
+		return online_prod_number,Online_Prod_Name,Online_Brand_Name,Comp_Prices,Comp_Program
 
 	def Base_Spider(self):
-		temp_df=self.df
-		URL_SCRAPED_FROM=[]
-		POST_CODE=[]		
-		DEPARTMENT_NAME=[]
-		CAT_NAME=[]
-		CAT_COUNT=[]
-		CATEGORY_URL_LIST=[]
-		DEPARTMENT_NAME_LIST=[]
-		URL_SCRAPED_FROM_LIST=[]
 		
+		CATEGORY_URL_LIST=[]
+
 		if (self.Is_Coles_Running()==1):
 			pass
 		else:
 			raise Exception ("Coles Website Down")
 			sys.exit(1)         
 		
+		print(f"Extracting Data for - {self.df['Site']}")
+		ua=UserAgent()
+#		random_user_agent=ua.random
+		random_user_agent=ua['google chrome']
+
+		print(random_user_agent)
+
 		options = webdriver.ChromeOptions()
 		options.add_argument('--disable-blink-features=AutomationControlled')
-		options.add_argument('--headless')
+#		options.add_argument(f'user-agent={random_user_agent}')
+#		options.add_argument('--headless')
 		options.add_argument('log-level=3')
+
+		
 		self.browser = webdriver.Chrome( executable_path=CHROME_DRIVER_PATH,options=options)
 		self.browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-
-
-		for index,url in enumerate(temp_df['URL']):
-			print(f"Extracting Data for - {temp_df['Site'][index]}")
-			self.browser.get(utils.Adjust_URL(url))                                          # Going by Store 
-			sleep(random.choice(IMPLICIT_WAIT))                                     # Including implict waits to tricke the bot
-			DEPARTMENT_URL=self.Get_Department_URL(soup(self.browser.page_source,features="lxml"))
-			for url in DEPARTMENT_URL:
+		self.browser.get(utils.Adjust_URL(self.df['URL']))                                          # Going by Store 
+		sleep(random.choice(IMPLICIT_WAIT))                                  						# Including implict waits to tricke the bot
+		DEPARTMENT_URL=self.Get_Department_URL(soup(self.browser.page_source,features="lxml"))
+		for index,url in enumerate(DEPARTMENT_URL):
+			if index>5:
+				break
+			else:
+#				self.browser.get('https://shop.coles.com.au/a/wentworth-point/everything/browse/convenience-meals-4390554?pageNumber=1')
 				self.browser.get(utils.URL_Adjustment_Categories(url))
-				sleep(3)
-				all_data=self.Get_Category_URL(soup(self.browser.page_source,features="lxml"),temp_df['Postcode'][index],temp_df['Site'][index])
-				URL_SCRAPED_FROM.append(all_data[0])
-				POST_CODE.append(all_data[1])
-				DEPARTMENT_NAME.append(all_data[2])
-				CAT_NAME.append(all_data[3])
-				CAT_COUNT.append(all_data[4])
-				CATEGORY_URL_LIST.append(all_data[5])
-				
-
-			temp_listx_1=self.PopulateLists(URL_SCRAPED_FROM)		# URL_SCRAPED_FROM
-			temp_listx_2=self.PopulateLists(POST_CODE)				#POST_CODE
-			temp_listx_3=self.PopulateLists(DEPARTMENT_NAME)		#DEPARTMENT_NAME
-			temp_listx_4=self.PopulateLists(CAT_NAME)				#CATEGORY_NAME
-			temp_listx_5=self.PopulateLists(CAT_COUNT)				#CATEGORY_COUNT
-			temp_listx_6=self.PopulateLists(CATEGORY_URL_LIST)		#CATEGORY_URL_LIST
-			
-			Data_Dict={
-					"URL_SCRAPED_FROM":temp_listx_1,
-					"POST_CODE":temp_listx_2,
-					"DEPARTMENT":temp_listx_3,
-					"CATEGORY_NAME":temp_listx_4,
-					"CATEGORY_COUNT":temp_listx_5,
-					"CATEGORY_URL":temp_listx_6
-					}
-			df=pd.DataFrame(Data_Dict)
-			df.to_csv(OUTPUT_FOLDER+"\\"+temp_df['Site'][index]+".csv")
-			print(f"{temp_df['Site'][index]} - Data Exported Successfully - Remaining {len(temp_df['Site'])-index}")
-
-			URL_SCRAPED_FROM.clear()
-			POST_CODE.clear()
-			DEPARTMENT_NAME.clear()
-			CAT_NAME.clear()
-			CAT_COUNT.clear()
-			CATEGORY_URL_LIST.clear()
-			Data_Dict.clear()
-
+				sleep(random.choice(IMPLICIT_WAIT)) 
+				all_data=self.Get_Category_URL(soup(self.browser.page_source,features="lxml"))
+				CATEGORY_URL_LIST=all_data[3]
+				for sub_cat_url in CATEGORY_URL_LIST:
+					self.browser.get(utils.URL_Adjustment_Categories(sub_cat_url))
+					sleep(2)
+					sub_cat_data=self.Get_Sub_Cat_URL(soup(self.browser.page_source,features="lxml"))
+					for families_url in sub_cat_data[2]:
+						self.browser.get(families_url)
+						sleep(random.choice(IMPLICIT_WAIT)) 
+						product_info=self.Get_Product_Info(soup(self.browser.page_source,features="lxml"),families_url)
+						print(type(product_info))
+	
 		self.browser.quit()
 
+cc=pd.DataFrame(config.LOCATIONS)
+Coles_Spider_URL(cc.iloc[1])
 
 
 
